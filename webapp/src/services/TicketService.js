a5314e891128bf5b5482a393946264f18b0e3f06
PicusViridis <kitsutsuki@hotmail.fr>
2019-10-04 18:44:38 +0200

    Replace less with sass and fix some style

--- webapp/src/services/TicketService.js
+++ webapp/src/services/TicketService.js
@@ -1,8 +1,16 @@
 // To be replaced with real API calls
-const TICKETS = [{ id: 1, state: 'to-do' }, { id: 2, state: 'in-progress' }, { id: 3, state: 'to-review' }, { id: 4, state: 'to-test' }, { id: 5, state: 'done' }]
+const TICKETS = [
+    { id: 1, state: 'to-do' },
+    { id: 2, state: 'to-do' },
+    { id: 3, state: 'in-progress' },
+    { id: 4, state: 'to-review' },
+    { id: 5, state: 'to-test' },
+    { id: 6, state: 'done' }
+]
 const TICKETS_DETAIL = {
     1: { id: 1, state: 'to-do', type: 'story', summary: 'Do something', sp: 5, assignees: [1, 2] },
-    2: {
+    2: { id: 2, state: 'to-do', type: 'bug', summary: 'Fix something', sp: 0, assignees: [1] },
+    3: {
         id: 2,
         state: 'in-progress',
         type: 'task',
@@ -11,9 +19,9 @@ const TICKETS_DETAIL = {
         sp: 2,
         assignees: [3]
     },
-    3: { id: 3, state: 'to-review', type: 'bug', summary: 'Fix something', sp: 0, assignees: [4] },
-    4: { id: 4, state: 'to-test', type: 'bug', summary: 'Fix something', sp: 0, assignees: [1] },
-    5: { id: 5, state: 'done', type: 'bug', summary: 'Fix something', sp: 0, assignees: [2] }
+    4: { id: 3, state: 'to-review', type: 'bug', summary: 'Fix something', sp: 0, assignees: [4] },
+    5: { id: 4, state: 'to-test', type: 'task', summary: 'Do something', sp: 2, assignees: [3] },
+    6: { id: 5, state: 'done', type: 'bug', summary: 'Fix something', sp: 0, assignees: [2] }
 }
 
 class TicketService {
