89c0b00161c4e3face837550cb1bebe45bc482a9
PicusViridis <kitsutsuki@hotmail.fr>
2019-10-23 22:52:06 +0200

    Use blueprint for ticket summary editor

--- webapp/src/ui/components/ticket-summary/TicketSummary.js
+++ webapp/src/ui/components/ticket-summary/TicketSummary.js
@@ -1,11 +1,12 @@
 import './TicketSummary.css'
 
+import { InputGroup } from '@blueprintjs/core'
 import PropTypes from 'prop-types'
 import React, { memo } from 'react'
 
 const TicketSummary = ({ editing, summary, onChange }) => {
     if (!editing) return <div className="summary">{summary}</div>
-    return <textarea className="summary-editor" value={summary} onChange={onChange} />
+    return <InputGroup className="ticket-summary-editor" value={summary} onChange={onChange} placeholder="Summary" />
 }
 
 TicketSummary.propTypes = {
