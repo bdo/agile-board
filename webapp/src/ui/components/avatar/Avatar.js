8ab8f0a70d5abd61bf592c266dd810dd659738ac
PicusViridis <kitsutsuki@hotmail.fr>
2019-10-23 22:50:25 +0200

    Remove avatar alt

--- webapp/src/ui/components/avatar/Avatar.js
+++ webapp/src/ui/components/avatar/Avatar.js
@@ -3,7 +3,7 @@ import './Avatar.css'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Avatar = ({ user, onClick }) => <img className="avatar" src={`/images/avatar/${user.id}.png`} alt={user.name} title={user.name} width={24} onClick={onClick} />
+const Avatar = ({ user, onClick }) => <img className="avatar" src={`/images/avatar/${user.id}.png`} alt="" title={user.name} width={24} onClick={onClick} />
 
 Avatar.propTypes = {
     user: PropTypes.object.isRequired,
